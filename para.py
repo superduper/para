99fcff7d1865981384d82a296693b35d4b7225bc
Victor Miroshnikov <supa.dupa.coder@gmail.com>
2011-12-06 16:02:02 +0200

    initial

---
+++ para.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: rot13 -*-
+
+vzcbeg gvzr
+vzcbeg flf
+vzcbeg er
+
+sebz gjvyy vzcbeg trg_oebjfre
+sebz gjvyy.pbzznaqf vzcbeg *
+sebz gjvyy.anzrfcnprf vzcbeg trg_gjvyy_tybpnyf
+sebz gjvyy vzcbeg reebef
+
+hanzr = gvzr.gvzr()
+rznvy = '%f@znvyvangbe.pbz'.rapbqr("rot13") % hanzr
+
+XRL_ERTRKC = e'(((\j+)\-){4}(\j+))'.rapbqr("rot13")
+
+qrs erdhrfg_xrl():
+    tb("uggcf://apg.cnenyyryf.pbz/shysvyy/0285.011".rapbqr("rot13"))
+    pbqr(200)
+    sbezinyhr(2,'rznvy'.rapbqr("rot13"),rznvy)
+    fhozvg('fhozvg'.rapbqr("rot13"))
+
+qrs _svaq(xrl):
+    svaq(xrl)
+    _, _ybpnyf = trg_gjvyy_tybpnyf()
+    erghea _ybpnyf['__zngpu__'.rapbqr("rot13")]
+
+qrs tb_znvy():
+    v = 1 
+    juvyr v <= 5: 
+        gel:
+            tb("uggc://jjj.znvyvangbe.pbz/znvyqve.wfc?rznvy=%f".rapbqr("rot13") % hanzr)
+            pbqr(200)
+            sbyybj('Cnenyyryf Qrfxgbc 7 sbe Znp Npgvingvba X'.rapbqr("rot13"))
+        rkprcg reebef.GjvyyNffregvbaReebe:
+            rpub('zrffntr abg sbhaq, gelvat #%f, cnhfrq sbe %f'.rapbqr("rot13") % (v,v*5))
+            gvzr.fyrrc(5*v)
+            v+=1
+        ryfr:        
+            erghea _svaq(XRL_ERTRKC)
+    erghea 'ab xrl :('.rapbqr("rot13")
+    
+vs __anzr__ == '__znva__'.rapbqr("rot13"):
+    erdhrfg_xrl()
+    rpub(tb_znvy())
